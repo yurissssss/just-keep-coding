194a921c254506391af30acf382f6ab8c411af8f
yurissssss <yur1n9@kakao.com>
2025-05-08 17:19:16 +0900

    ✏️ update : Student.java 05-08

--- 03 이것이 자바다/12_스트림요소처리/Basic/src/ch17/sec03/Student.java
+++ 03 이것이 자바다/12_스트림요소처리/Basic/src/ch17/sec03/Student.java
@@ -4,5 +4,15 @@ public class Student {
     private String name;
     private int score;
 
+    public Student (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
 
+    public String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
+    }
 }
