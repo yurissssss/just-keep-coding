e1a03e9f7846909a694e7883cefbddcaff826563
yurissssss <yur1n9@kakao.com>
2025-05-08 23:17:09 +0900

    📁 add : StreamExample.java 05-08

---
+++ 03 이것이 자바다/12_스트림요소처리/Basic/src/ch17/ch17/sec04/exam02/StreamExample.java
@@ -0,0 +1,32 @@
+package ch17.ch17.sec04.exam02;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        String[] strArray = {"홍길동", "신용권", "김미나"};
+        Stream<String> strStream = Arrays.stream(strArray);
+        //마지막은 ',' 출력 안 하기
+        strStream.forEach(item -> {
+            if (item == strArray[2]) {
+                System.out.println(item);
+            } else {
+                System.out.print(item + ",");
+            }
+        });
+        System.out.println();
+
+        int[] intArray = {1, 2, 3, 4, 5};
+        IntStream intStream = Arrays.stream(intArray);
+        //마지막은 ',' 출력 안 하기
+        intStream.forEach(item -> {
+            if (item == intArray[4]) {
+                System.out.println(item);
+            } else {
+                System.out.print(item + ",");
+            }
+        });
+    }
+}
