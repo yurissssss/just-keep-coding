5b61eb3ab684faf6560575597ff0ab03005ca45b
yurissssss <yur1n9@kakao.com>
2025-05-08 17:36:08 +0900

    .

---
+++ 03 이것이 자바다/12_스트림요소처리/Basic/src/ch17/sec04/exam01/Product.java
@@ -0,0 +1,64 @@
+package ch17.sec04.exam01;
+
+public class Product {
+    private int pno;
+    private String name;
+    private String company;
+    private int price;
+
+    //생성자 - 롬복 / Generate
+    public Product(int pno, String name, String company, int price) {
+        this.pno = pno;
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
+
+    //Getter - 롬복 / Generate
+
+    public int getPno() {
+        return pno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    //Setter - 롬복 / Generate
+
+    public void setPno(int pno) {
+        this.pno = pno;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    //toString - 롬복 / Generate
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "pno=" + pno +
+                ", name='" + name + '\'' +
+                ", company='" + company + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
