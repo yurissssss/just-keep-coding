722bb8356343ddd91c38a68a808b22610d10a19e
yurissssss <yur1n9@kakao.com>
2025-05-11 00:40:32 +0900

    📁 add : HomeExample.java 05-10

---
+++ 03 이것이 자바다/06_중첩객체/Basic/src/ch09/sec07/exam02/HomeExample.java
@@ -0,0 +1,11 @@
+package ch09.sec07.exam02;
+
+public class HomeExample {
+    public static void main(String[] args) {
+        //Home 객체 생성
+        Home home = new Home();
+
+        //익명 구현 객체가 대입된 필드 사용
+        home.use1();
+    }
+}
