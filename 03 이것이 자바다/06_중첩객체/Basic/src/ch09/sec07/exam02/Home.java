98e2165303277d5d317ac849f4cc78597d1703cf
yurissssss <yur1n9@kakao.com>
2025-05-11 00:40:26 +0900

    📁 add : Home.java 05-10

---
+++ 03 이것이 자바다/06_중첩객체/Basic/src/ch09/sec07/exam02/Home.java
@@ -0,0 +1,23 @@
+package ch09.sec07.exam02;
+
+public class Home {
+    //필드에 익명 구현 객체 대입
+    private RemoteControl rc = new RemoteControl() {
+        @Override
+        public void turnOn() {
+            System.out.println("TV를 켭니다.");
+        }
+        @Override
+        public void turnOff() {
+            System.out.println("TV를 끕니다.");
+        }
+    };
+
+    //메소드(필드 이용)
+    public void use1() {
+        rc.turnOn();
+        rc.turnOff();
+    }
+}
+
+
