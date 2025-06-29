13dd7c83622d7390eac11cc06e391ccd3ecd8bd2
yurissssss <yur1n9@kakao.com>
2025-06-29 17:53:50 +0900

    ✏️ update: 01_spring_vue 수정 06-29

--- 08 Spring - Vue 연동/01_프로젝트_준비/basic_advanced/project/frontend/src/App.vue
+++ 08 Spring - Vue 연동/01_프로젝트_준비/basic_advanced/project/frontend/src/App.vue
@@ -4,6 +4,7 @@ import DefaultLayout from './components/layouts/DefaultLayout.vue';
 </script>
 
 <template>
+  <!-- DefaultLayout: header / main / footer 항상 보이도록 설정 -->
   <DefaultLayout>
     <RouterView />
   </DefaultLayout>
--- 08 Spring - Vue 연동/01_프로젝트_준비/basic_advanced/project/frontend/src/components/layouts/DefaultLayout.vue
+++ 08 Spring - Vue 연동/01_프로젝트_준비/basic_advanced/project/frontend/src/components/layouts/DefaultLayout.vue
@@ -8,6 +8,8 @@ import Footer from './Footer.vue';
     <Header />
     <NavBar />
     <div class="content my-5 px-3">
+      <!-- slot: 부모의 내용을 받아 넣는 자리
+                어떤 component라도 다 삽입 가능 -> 결합도 낮춤 -->
       <slot></slot>
     </div>
     <Footer />
